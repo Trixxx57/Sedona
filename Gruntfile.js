e30544584a732cb97ff4ceadd0503ef8455f0f79
Trixxx57 <kirenkoalex@gmail.com>
2019-11-25 23:56:00 +0200

    attempt1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-less");
-	
+	grunt.loadNpmTasks("grunt-browser-sync");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
   grunt.initConfig({
     less: {
     	style: {
@@ -8,7 +10,35 @@ module.exports = function(grunt) {
     			"css/style.css": "less/*.less"
     		}
       }
+    },
+
+  browserSync: {
+    server: {
+    	bsFiles: {
+    		 src: [
+    		 	"*.html",
+    		 	"css/*.css",
+    		 ]        			
+    		},
+    		options: {
+    			server: ".",
+    			watchTask: true,
+    			notify: false,
+    			open: true,
+    			cors: true,
+    			ui: false
+    		}
+    	}
+    },
+
+    watch: {
+    	style: {
+    		files: ["less/**/*.less"],
+    		tasks: ["less", "postcss"]
+    	}
     }
-   });
-	};
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+};
  
