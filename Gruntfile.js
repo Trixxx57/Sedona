af8b83ff0b5804781fa7554f1a7532fd7d7ab3dc
Trixxx57 <kirenkoalex@gmail.com>
2019-12-08 21:53:08 +0200

    Начал делать планшетную версию

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
     server: {
     	bsFiles: {
     		 src: [
-    		 	"src/sedona-photo-mobile.html",
+    		 	"src/index.html",
     		 	"src/css/*.css"
     		 ]        			
     		},
